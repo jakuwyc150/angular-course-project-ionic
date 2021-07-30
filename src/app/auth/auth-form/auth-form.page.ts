e1081b11240105f54607f9cd2915c4d4361661b0
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-30 15:48:27 +0200

    Added authentication form

--- src/app/auth/auth-form/auth-form.page.ts
+++ src/app/auth/auth-form/auth-form.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../store/app.reducer';
+import * as AuthActions from '../store/auth.actions';
 
 @Component({
   selector: 'app-auth-form',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-form.page.scss'],
 })
 export class AuthFormPage implements OnInit {
+  authForm: FormGroup;
+  error: string;
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController,
+    private formBuilder: FormBuilder,
+    private store: Store<fromRoot.AppState>
+  ) {}
 
   ngOnInit() {
+    this.initForm();
+
+    this.store.select('auth').subscribe(async (authState) => {
+      this.error = authState.authError;
+      this.isLoading = authState.loading;
+
+      if (this.error) {
+        await this.alertController.create({
+          buttons: ['OK'],
+          header: 'Authentication failed',
+          message: this.error
+        });
+      }
+    });
   }
 
+  formSubmit() {
+    this.store.dispatch(AuthActions.loginStart({
+      email: this.authForm.value.userEmail,
+      password: this.authForm.value.userPassword
+    }));
+
+    this.authForm.reset();
+  }
+
+  private initForm() {
+    this.authForm = this.formBuilder.group({
+      userEmail: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+
+      userPassword: ['', [
+        Validators.required,
+        Validators.minLength(3)
+      ]]
+    });
+  }
 }
